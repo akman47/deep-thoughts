1db2ea48669dfcd28b592a7a77db1690ce74d5e0
Angela Man <angela.k.man@gmail.com>
2021-07-26 12:53:53 -0500

    implement jwt and auth middleware

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -28,7 +28,13 @@ const typeDefs = gql`
         username: String
     }
 
+    type Auth {
+        token: ID!
+        user: User
+    }
+
     type Query {
+        me: User
         users: [User]
         user(username: String!): User
         thoughts(username: String): [Thought]
@@ -36,8 +42,8 @@ const typeDefs = gql`
     }
 
     type Mutation {
-        login(email: String!, password: String!): User
-        addUser(username: String!, email: String!, password: String!): User
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
     }
 `;
 
